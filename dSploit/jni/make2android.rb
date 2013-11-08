d9e1c67147d0b9e31031e161ad16a09d756d489c
tux-mind <massimo.dragano@gmail.com>
2013-11-08 05:01:46 +0100

    make2android: deprecated libtool, will now use only generic checks. TODO: drop extra libraries given by libtool

--- dSploit/jni/make2android.rb
+++ dSploit/jni/make2android.rb
@@ -1,4 +1,4 @@
-#!/bin/env ruby
+#!/usr/bin/env ruby
 
 # This file is part of the dSploit.
 # 
@@ -37,7 +37,7 @@ class ObjectTarget
 	end
 	
 	def ==(o)
-		o.class == self.class && o.name == self.name
+		o.class == self.class and o.name == self.name
 	end
 end
 
@@ -47,8 +47,6 @@ class Library < ObjectTarget
 	def initialize()
 		super
 		@unknown=[]
-		@includes=[]
-		@cflags=[]
 		@warned=false
 		@shared=false
 		@srcs=nil
@@ -61,6 +59,14 @@ class Library < ObjectTarget
 	def libname
 		@name.sub(/.*\//,'').sub(/\..*$/,'')
 	end
+end
+
+class Executable < Library
+
+    def exename
+        libname
+    end
+
 end
 
 ##static variables...
@@ -88,14 +94,41 @@ end
 def system_includes
 	/sources\/cxx-stl\//
 end
+def extra_cflags
+    ["-static","-ffunction-sections", "-fdata-sections"]
+end
+def extra_ldflags
+    [" -Wl,--gc-sections"]
+end
 def cd_re
-	/^cd (?<dir>[^; ]+)/
+	/^cd\s+(?<dir>[^; ]+)/
 end
 def pwd_re
 	/#{Dir.pwd}\//
 end
 def cmake_cd_re
-	/^cd [^ ]+ +&& +[^ ]+cmake /
+	/^cd\s+[^\s]+\s+&&\s+[^ ]+cmake\s/
+end
+
+# directory object
+class Directory
+    attr_accessor :absolute_path
+    attr_reader :relative_path,:top_relative_path
+
+    def initialize( abs )
+        @absolute_path=abs
+        @absolute_path+='/' if absolute_path[-2] != '/'
+        @relative_path=absolute_path.sub(pwd_re,'')
+        @top_relative_path=absolute_path.sub(topwd_re,'')
+    end
+
+    def ==(o)
+        o.class == self.class && o.absolute_path == this.absolute_path
+    end
+
+    def to_s
+        absolute_path
+    end
 end
 
 def get_params(line,t)
@@ -108,17 +141,13 @@ def get_params(line,t)
 		dst << flag[0] if !(dst.include? flag[0])
 	end
 	line.scan(include_re).each do |inc|
-		if(inc[0][0] == '/')
-			next if inc[0] =~ system_includes
-			abs=File.absolute_path(inc[0])
-		else
-			abs=File.absolute_path($make_abs_dir + inc[0])
+		next if inc[0] =~ system_includes
+		dir=get_absolute_path(inc[0])
+		while File.symlink? dir do
+			dir=File.expand_path(File.readlink(dir),File.dirname(dir))
 		end
-		while File.symlink? abs do
-			abs=File.expand_path(File.readlink(abs),File.dirname(abs))
-		end
-		next if !(File.exists? abs)
-		rel=abs.sub(topwd_re,'')
+		next if !(File.exists? dir)
+		rel=dir.sub(topwd_re,'')
 		t.includes << rel if !(t.includes.include? rel)
 	end
 	line.scan(libs_re).each do |lib|
@@ -131,42 +160,42 @@ end
 
 def bash_globbing(t,inputs)
 	globbed=inputs.find_all{|i| i =~ /\*.*\.[a-z]?[oa]/}
-	inputs= inputs - globbed
+	# cannot use -=, must work on the same object
+	inputs.delete_if{|i| globbed.include? i}
 	globbed.each do |g|
 		#transalte bash globbing into ruby regex and
 		#add current make working dir to gobbing
-		g=$make_dir+g.gsub(".","\.").gsub("*",".*").gsub("/","\/")
+		g=($dir_stack[-1].relative_path+g).gsub(".","\.").gsub("*",".*").gsub("/","\/")
 		found = $list.find_all{|item| item.name =~ /#{g}/}
 		if found.size == 0 then
 			t.unknown << g
 		else
-			found.each {|item| inputs << item.name}
+			found.each {|item| inputs << File.absolute_path($dir_stack[0].absolute_path + item.name)}
 		end
 	end
 end
 
 def parse_inputs(t,inputs)
 	inputs.each do |input|
-		if input[0] == '/'
-			abs=input
-		else
-			abs=File.absolute_path($make_abs_dir + input)
-		end
-		input=abs.sub(pwd_re,'')
+	    input=get_absolute_path(input)
+	    input=input.sub(pwd_re,'')
 		case input
 		when /\.(c|cpp|cc|s|S)$/
 			abort("making library `#{t.name}' from source `#{input}'") if t.is_a?(Library)
+			# i known that you CAN build an executable from sources,
+			# but this script it's wrote for solving bug projects porting trouble,
+			# not simple printf("hello world!\n");
+			abort("making executable `#{t.name}' from source `#{input}'") if t.is_a?(Executable)
 			t.srcs << input
 		when /\.[la]?[oa]$/
-			f=$list.find_all{|item| item.name == input}
+			f=$list.find_all{|item| !(item.is_a?(Executable)) and item.name == input}
 			if f.size > 1
 				abort("#{t.name}: more targets for `#{input}' => #{f}")
 			elsif f.size == 0
 				abort("#{t.name}: cannot find `#{input}' in #{$list}")
 			end
 			i=f[0]
-			
-			if i.instance_of?(Library)
+			if i.is_a?(Library)
 				t.libs << i
 			else
 				t.objs << i
@@ -175,12 +204,13 @@ def parse_inputs(t,inputs)
 			fullname=$~[0]
 			name=$1.sub(/^lib/,'')
 			next if system_libs.include? name
-			f=$list.find_all{|item| item.name == fullname}
+			f=$list.find_all{|item| item.class != Executable and item.name == fullname}
 			if f.size > 1
 				abort("#{t.name}: more targets for `#{input}' => #{f}")
 			elsif f.size == 0
 				abort("#{t.name}: cannot find `#{input}' in #{$list}")
 			end
+			abort("#{t.name}: executable as input `#{f[0]}'") if f[0].is_a?(Executable)
 			t.libs << f[0]
 		else
 			abort("unrecognized input: #{input}")
@@ -192,35 +222,28 @@ def get_absolute_path(file)
 	if file[0] == '/' then
 		File.absolute_path(file)
 	else
-		File.absolute_path($make_abs_dir + file)
+		File.absolute_path($dir_stack[-1].absolute_path + file)
 	end
 end
 
+# check if we are changing directory for the current command only
 def check_cd(line)
+    res=false
 	if cd_re =~ line then
-		re_result = $~
+		$dir_stack.push Directory.new get_absolute_path($~[:dir])
 		# cd /path && /usr/bin/cmake ... => we will remain in that directory
-		$old_make_abs=$make_abs_dir if !(cmake_cd_re =~ line)
-		$make_abs_dir=get_absolute_path(re_result[:dir])+'/'
-		$make_dir=$make_abs_dir.sub(pwd_re,'')
+        if !(cmake_cd_re =~ line) then
+            res=true
+        end
 	end
-end
-
-def restore_dir
-	if $old_make_abs.size > 0 then
-		$make_abs_dir=$old_make_abs
-		$make_dir=$make_abs_dir.sub(pwd_re,'')
-		$old_make_abs=""
-	end
+	res
 end
 	
 
-$make_dir=""
-$make_abs_dir=""
-$old_make_abs=""
 $list=[]
-dir_re=/Entering directory `([^']+)'/
-lt_check=/(--mode=(?<mode>(link|compile))).*?(-o (?<target>[^ ]+\.([al]?o|l?a)))/
+$dir_stack=[]
+dir_re=/(?<action>(Entering|Leaving)) directory `(?<path>[^']+)'/
+lt_check=/(--mode=(?<mode>(link|compile))).*?-o (?<target>[^ ]+(\.([al]?o|l?a))?)/
 lt_get_inputs=/^[^-].*\.(c|cpp|cc|S|s|[la]?o|l?a|so(\.[0-9]+)*)$/
 # libtool compile some extra objects when making static libs
 # end it change object/source order upon libtool version...
@@ -232,56 +255,62 @@ generic_get_inputs=lt_get_inputs
 generic_get_target=/-o (?<target>[^ ]+\.([al]?o|l?a))( |$)/
 generic_check_executable=/-o [^ ]+($| )/
 
-lt_found=false
-
+in_temporary_dir=false
+n=0
 text=File.open(ARGV[0]).read
 text.gsub!(/\r\n?/, "\n")
 text.each_line do |line|
-	line.gsub!(/`.*?`/,'') # remove bash subshells
-	line.sub!(/^\[ *[0-9]+%\]\s+/,'') # remove cmake percentage status
-	restore_dir()
+  line.gsub!(/`.*?`/,'') # remove bash subshells
+  line.sub!(/^\[ *[0-9]+%\]\s+/,'') # remove cmake percentage status
+  n+=1
+  if in_temporary_dir then
+    $dir_stack.pop
+    in_temporary_dir=false
+  end
   if dir_re =~ line then
-	  $make_abs_dir=$1+'/'
-	  $make_dir=$make_abs_dir.sub(pwd_re,'')
-		next
-	else
-		check_cd(line)
-	end
-  if lt_check =~ line then
-		lt_found=true
-	  isLibrary=false
-		re_result=$~
-	  name=$make_dir+re_result[:target]
-	  next if $list.find_all{|item| item.name == name}.size > 0
-	  inputs=(line.split.grep lt_get_inputs).delete_if{|item| item == re_result[:target]}
-	  case name
-		when /\.[a-z]?o$/
-			t=ObjectTarget.new
-		when /\.[a-z]?a$/
-			t=Library.new
-			isLibrary=true
-		else
-			abort("unrecognized target `#{name}'")
-		end
-	  t.name = name
-		#check for bash globbing
-		bash_globbing(t,inputs) if isLibrary
-	  get_params(line,t)
-		parse_inputs(t,inputs)
-	  $list << t
-  elsif lt_extra_check =~ line then
-		lt_found=true
-	  t = ObjectTarget.new
-	  t.name = $make_dir+($~[:target].sub(".o",".lo"))
-	  #remove .libs
-	  t.name.sub!('.libs/','')
-	  next if $list.find_all{|item| item.name == t.name}.size > 0
-	  (lt_extra_get_inputs.match line)[:inputs].split.each do |s|
-		  t.srcs << File.absolute_path($make_abs_dir + s).sub(pwd_re,'')
-	  end
-	  get_params(line, t)
-	  $list << t
-	elsif !lt_found && generic_check =~ line then
+      case $~[:action]
+      when "Entering"
+        dir=Directory.new $~[:path]
+        $dir_stack.push(dir)
+      when "Leaving"
+        $dir_stack.pop
+      end
+  elsif check_cd(line) then
+      in_temporary_dir=true
+  end
+#  if false and lt_check =~ line then
+#      re_result=$~
+#	  name=$dir_stack[-1].relative_path+re_result[:target]
+#	  next if $list.find_all{|item| item.name == name}.size > 0
+#	  inputs=(line.split.grep lt_get_inputs).delete_if{|item| item == re_result[:target]}
+#	  case name
+#		when /\.[a-z]?o$/
+#			t=ObjectTarget.new
+#		when /\.[a-z]?a$/
+#			t=Library.new
+#		else
+#			t=Executable.new
+#		end
+#	  t.name = name
+#		#check for bash globbing
+#		bash_globbing(t,inputs) if t.is_a?(Library) or t.is_a?(Executable)
+#	  get_params(line,t)
+#		parse_inputs(t,inputs)
+#	  $list << t
+#  elsif false and lt_extra_check =~ line then
+#		lt_found=true
+#	  t = ObjectTarget.new
+#	  t.name = $dir_stack[-1].relative_path+($~[:target].sub(".o",".lo"))
+#	  #remove .libs
+#	  t.name.sub!('.libs/','')
+#	  next if $list.find_all{|item| item.name == t.name}.size > 0
+#	  (lt_extra_get_inputs.match line)[:inputs].split.each do |s|
+#		  t.srcs << get_absolute_path(s).sub(pwd_re,'')
+#	  end
+#	  get_params(line, t)
+#	  $list << t
+#	els
+	if generic_check =~ line then
 		prog=$~[:prog]
 		inputs=(line.split.grep generic_get_inputs)
 		res=[]
@@ -299,7 +328,6 @@ text.each_line do |line|
 			name = objs[0]
 			t.name = get_absolute_path(name).sub(pwd_re,'')
 			next if ($list.include? t)
-			#abort("make_abs_dir: #{$make_abs_dir}\nmake_dir: #{$make_dir}\nname: #{name}\nt.name: #{t.name}")
 			inputs.delete_if{|item| item == name}
 			get_params(line,t)
 			parse_inputs(t,inputs)
@@ -307,24 +335,28 @@ text.each_line do |line|
 		# probably we are building a library if no sources in input 
 		elsif (res=(inputs.grep /\.(l?a|so(\.[0-9]+)*)$/)).size > 0 then
 			# try to find the target library
-			shared=false
 			if prog=="ar" then # if using ar, the target is the first one
 				name=res[0]
+				t=Library.new
 			elsif line =~ /-o (?<target>[^ ]+\.l?a)/ then
 				name=$~[:target]
+				t=Library.new
 			elsif line =~ /-o (?<target>[^ ]+\.so(\.[0-9]+)*)/ then
 				name=$~[:target]
-				shared=true
-			elsif line =~ /-o [^ ]+/ then # executable
-				next
+				t=Library.new
+				t.shared=true
+			elsif line =~ /-o (?<target>[^ ]+)/ then
+				name=$~[:target]
+				t=Executable.new
 			else
 				abort("cannot find library target for `#{line}'")
 			end
-			t = Library.new
-			t.shared=shared
 			t.name = get_absolute_path(name).sub(pwd_re,'')
 			next if ($list.include? t)
-			inputs=inputs.delete_if{|item| item == name}
+			#next if (t.is_a?(Library) and $list.select{|item| item.is_a?(Library) and item.libname == t.libname}.size > 0)
+			inputs.delete_if{|item| item == name}
+			#check for bash globbing
+			bash_globbing(t,inputs)
 			get_params(line,t)
 			parse_inputs(t,inputs)
 			$list << t
@@ -333,12 +365,12 @@ text.each_line do |line|
 		end
   end
 end
-$list.find_all{|item| item.instance_of?(Library)}.each do |t|
+$list.find_all{|item| item.is_a?(Library) or item.is_a?(Executable)}.each do |t|
 	t.unknown.each do |u|
-		found=$list.find_all{|item| item.name =~ /#{u}/}
+		found=$list.find_all{|item| !(item.is_a?(Executable)) and item.name =~ /#{u}/}
 		abort("cannot find `#{u}' in `#{$list}'") if found.size == 0
 		found.each do |f|
-			if f.instance_of?(Library)
+			if f.is_a?(Library)
 				t.libs << f
 			else
 				t.objs << f
@@ -346,19 +378,18 @@ $list.find_all{|item| item.instance_of?(Library)}.each do |t|
 		end
 	end
 	next if t.warned
-	libs=$list.find_all{|item| item != t && item.instance_of?(Library) && !(item.warned) && item.libname == t.libname}
-	warn "more libraries has the same name:" if libs.size > 0
-	warn "#{t.libname} from `#{t.name}'" if libs.size > 0
+	t.warned = true
+	libs=$list.find_all{|item| t.class == item.class and t.libname == item.libname and !(item.warned)}
+	warn "more libraries has the same name:\n#{t.libname} from `#{t.name}'" if libs.size > 0
 	libs.each do |l|
 		warn "#{l.libname} from `#{l.name}'"
 		l.warned=true
 	end
-	t.warned = true
 end
 
 print "LOCAL_PATH := $(call my-dir)\n"
 
-$list.find_all{|item| item.instance_of?(Library)}.each do |lib|
+$list.find_all{|item| item.is_a?(Library) or item.is_a?(Executable)}.each do |lib|
 	print "\n#original path: #{lib.name}\n"
 	print "include $(CLEAR_VARS)\n"
 	includes=[]
@@ -398,10 +429,13 @@ $list.find_all{|item| item.instance_of?(Library)}.each do |lib|
 		i+=flag.size+3
 		print "#{flag} "
 	end
+    # fixed stuff ( you can change it from the head of this script )
+	print "\n# fixed flags\nLOCAL_CFLAGS+= #{extra_cflags.join(" ")}" if extra_cflags.size > 0
+
 	# the whole world call it CXXFLAGS, but google decided that in their makefiles
 	# CXXFLAGS is CPPFLAGS ( that is usually the C/C++ preprocessor flags )
 	# /me FACEPALM
-	print "\nLOCAL_CPPFLAGS:= " if cxxflags.size > 0
+	print "\n\nLOCAL_CPPFLAGS:= " if cxxflags.size > 0
 	i=17
 	cxxflags.each do |flag|
 		if (i+flag.size) > 80 && i > 0 then
@@ -411,6 +445,7 @@ $list.find_all{|item| item.instance_of?(Library)}.each do |lib|
 		i+=flag.size+3
 		print "#{flag} "
 	end
+	print "\n\nLOCAL_LDFLAGS:= #{extra_ldflags.join(" ")}" if lib.is_a?(Executable) and extra_ldflags.size > 0
 	print "\n\n"
 	print "LOCAL_C_INCLUDES:= " if includes.size > 0
 	includes.each do |inc|
@@ -420,12 +455,16 @@ $list.find_all{|item| item.instance_of?(Library)}.each do |lib|
 	srcs.each do |s|
 		print "\\\n\t#{s}"
 	end
+
 	print "\nLOCAL_STATIC_LIBRARIES:= " if libs.size > 0
 	libs.each do |l|
 		print "\\\n\t#{l.libname}"
 	end
 	print "\nLOCAL_MODULE := #{lib.libname}\n\n"
-	if lib.shared
+	#TODO: refactoring
+	if lib.is_a?(Executable)
+	    print "include $(BUILD_EXECUTABLE)\n\n"
+	elsif lib.shared
 		print "include $(BUILD_SHARED_LIBRARY)\n\n"
 	else
 		print "include $(BUILD_STATIC_LIBRARY)\n\n"
