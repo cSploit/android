6162b71865152a428b60e9006e07fca33aa422ef
tux-mind <massimo.dragano@gmail.com>
2014-04-15 20:10:59 +0200

    fixed find command removed extra slashes

--- nightly-build.sh
+++ nightly-build.sh
@@ -18,7 +18,7 @@ DATE=`date +%Y-%m-%d`
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LOG_DIR="${DIR}/logs/"
 MAX_DAYS="15"
-PREVIOUS_COMMIT=$(cat "${LOG_DIR}/last_commit")
+PREVIOUS_COMMIT=$(cat "${LOG_DIR}last_commit")
 
 if [ ! -d "${LOG_DIR}" ]; then
     mkdir -p $LOG_DIR
@@ -32,8 +32,8 @@ cd "${DIR}" || exit 1 | tee $LOG_DIR/$DATE.log
 
 echo -n -e "${YELLOW}Cleaning old files${RESET}\n" | tee $LOG_DIR/$DATE.log
 LAST_APK=$(readlink -f "${NIGHTLIES_OUT_DIR}/dSploit-lastest.apk")
-find $NIGHTLIES_OUT_DIR -type f -a -ctime -"${MAX_DAYS}" -a ! -name "${LAST_APK}" -delete &>1 | tee $LOG_DIR/$DATE.log
-find $LOG_DIR -type f -a -ctime -"${MAX_DAYS}" -delete &>1 | tee $LOG_DIR/$DATE.log
+find $NIGHTLIES_OUT_DIR -type f -a -mtime +${MAX_DAYS} -a ! -name "${LAST_APK}" -exec rm -f "{}" \; | tee $LOG_DIR/$DATE.log
+find $LOG_DIR -type f -a -mtime +${MAX_DAYS} -exec rm -f "{}" \; | tee $LOG_DIR/$DATE.log
 
 echo -n -e "${CYAN}Syncing git repo...${RESET}\n" | tee $LOG_DIR/$DATE.log
 git pull | tee $LOG_DIR/$DATE.log
@@ -54,6 +54,6 @@ rm -f dSploit/build/apk/dSploit-release.apk
 echo -n -e "${GREEN}Copying signed apk to output directory${RESET}\n" | tee $LOG_DIR/$DATE.log
 cp dSploit/build/apk/dSploit-release.apk $NIGHTLIES_OUT_DIR/dSploit-$LAST_COMMIT.apk &&
 ln -sf "dSploit-${LAST_COMMIT}.apk" $NIGHTLIES_OUT_DIR/dSploit-lastest.apk &&
-echo "${LAST_COMMIT}" > "${LOG_DIR}/last_commit" | tee $LOG_DIR/$DATE.log
+echo "${LAST_COMMIT}" > "${LOG_DIR}last_commit" | tee $LOG_DIR/$DATE.log
 
 echo -n -e "${GREEN}Done.${RESET}\n\n" | tee $LOG_DIR/$DATE.log
