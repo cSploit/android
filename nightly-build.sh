62b14c1b098655ad142243ff8e4b739537b84e97
tux-mind <massimo.dragano@gmail.com>
2014-04-15 16:46:16 +0200

    optimized nightly-build.sh
    we chack that a commit was made by the last one
    clean older builds and logs

--- nightly-build.sh
+++ nightly-build.sh
@@ -17,6 +17,8 @@ RESET="\\e[0m"
 DATE=`date +%Y-%m-%d`
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LOG_DIR="${DIR}/logs/"
+MAX_DAYS="15"
+PREVIOUS_COMMIT=$(cat "${LOG_DIR}/last_commit")
 
 if [ ! -d "${LOG_DIR}" ]; then
     mkdir -p $LOG_DIR
@@ -26,12 +28,32 @@ if [ ! -d "${NIGHTLIES_OUT_DIR}" ]; then
     mkdir -p $NIGHTLIES_OUT_DIR
 fi
 
+cd "${DIR}" || exit 1 | tee $LOG_DIR/$DATE.log
+
+echo -n -e "${YELLOW}Cleaning old files${RESET}\n" | tee $LOG_DIR/$DATE.log
+LAST_APK=$(readlink -f "${NIGHTLIES_OUT_DIR}/dSploit-lastest.apk")
+find $NIGHTLIES_OUT_DIR -type f -a -ctime -"${MAX_DAYS}" -a ! -name "${LAST_APK}" -delete &>1 | tee $LOG_DIR/$DATE.log
+find $LOG_DIR -type f -a -ctime -"${MAX_DAYS}" -delete &>1 | tee $LOG_DIR/$DATE.log
+
+echo -n -e "${CYAN}Syncing git repo...${RESET}\n" | tee $LOG_DIR/$DATE.log
+git pull | tee $LOG_DIR/$DATE.log
+
+LAST_COMMIT=$(git rev-parse HEAD)
+
+if [ -n "${PREVIOUS_COMMIT}" -a "${PREVIOUS_COMMIT}" == "${LAST_COMMIT}" ]; then
+    echo -n -e "${YELLOW}Nothing changed${RESET}" | tee $LOG_DIR/$DATE.log
+    echo -n -e "${GREEN}Done" | tee $LOG_DIR/$DATE.log
+    exit 0
+fi
+
 echo -n -e "${CYAN}Building dSploit...${RESET}\n" | tee $LOG_DIR/$DATE.log
+rm -f dSploit/build/apk/dSploit-release.apk
 ./gradlew clean | tee $LOG_DIR/$DATE.log
 ./gradlew assembleRelease | tee $LOG_DIR/$DATE.log
 
-echo -n -e "${GREEN}Copying signed apk to output directory${RESET} ('NIGHTLIES_OUT_DIR' environment variable)\n" | tee $LOG_DIR/$DATE.log
-cp $DIR/dSploit/build/apk/dSploit-release.apk $NIGHTLIES_OUT_DIR/dSploit-$DATE-nightly.apk | tee $LOG_DIR/$DATE.log
+echo -n -e "${GREEN}Copying signed apk to output directory${RESET}\n" | tee $LOG_DIR/$DATE.log
+cp dSploit/build/apk/dSploit-release.apk $NIGHTLIES_OUT_DIR/dSploit-$LAST_COMMIT.apk &&
+ln -sf "dSploit-${LAST_COMMIT}.apk" $NIGHTLIES_OUT_DIR/dSploit-lastest.apk &&
+echo "${LAST_COMMIT}" > "${LOG_DIR}/last_commit" | tee $LOG_DIR/$DATE.log
 
 echo -n -e "${GREEN}Done.${RESET}\n\n" | tee $LOG_DIR/$DATE.log
-
