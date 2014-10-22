a8f5bffd00afa65eb84757a6fa70147566db8f52
tux-mind <massimo.dragano@gmail.com>
2014-10-22 15:19:28 +0200

    forgot to commit this file for fixing method signature

--- dSploit/jni/dSploitClient/command.h
+++ dSploit/jni/dSploitClient/command.h
@@ -8,6 +8,6 @@
 
 int on_command(JNIEnv *, message *);
 int start_blind_command(JNIEnv *, jclass, jstring);
-int start_command(JNIEnv *, jclass, jstring, jstring, jstring);
+int start_command(JNIEnv *, jclass, jstring, jstring);
 void kill_child(JNIEnv *, jclass, int, int);
 #endif
