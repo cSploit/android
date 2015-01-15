fecc5832b9cd9b0db4adb76bfd6989c7fccf3c5c
tux-mind <massimo.dragano@gmail.com>
2015-01-15 23:54:13 +0100

    no way to fix #1 for now

--- cSploit/jni/known-issues/start_daemon.sh
+++ cSploit/jni/known-issues/start_daemon.sh
@@ -4,14 +4,6 @@ exec 3<> ./start_daemon.log
 
 script_dir="$(readlink /proc/$$/cwd )"
 
-die1() {
-  cd "$script_dir"
-  grep -q "/cSploit" /proc/mounts && umount /cSploit
-  test -d /cSploit && { mount -o remount,rw /; rmdir /cSploit; mount -o remount,ro / ;}
-  echo "$1" >&3
-  exit 1
-}
-
 test -f ./issues || { ./known-issues > ./issues 2>&3 ; chmod 777 ./issues ;}
 
 while read issue; do
@@ -20,14 +12,9 @@ while read issue; do
 
   case issue in
     1)
-       if [ ! -f "/cSploit/cSploitd" ]; then
-          mount -o remount,rw / 2>&3 || die1 "remount rw failed"
-          test -d "/cSploit" || mkdir "/cSploit" 2>&3 || die1 "mkdir failed"
-          mount -o bind "$script_dir" "/cSploit" 2>&3 || die1 "bind failed"
-          mount -o remount,ro / 2>&3 || echo "remount ro failed" >&3
-        fi
-        cd /cSploit 2>&3 || die1 "chdir failed"
-        ;;
+       echo "issue #1 found, cSploit will not be started" >&2
+       exit 1
+       ;;
      
    esac
 done < ./issues
