cSploit/android

commit 9eb34156d93543c60b62c3c420551ec717a1513e

UI/Build upgrade: Android Studio, gradle, + material design
*  Builds w/latest Android Studio 1.2 preview 4
*  Use gradle 2.3 (File/Project Structure/Project tab)
*  Builds for API 9-22
*  material design -- note the current "Dark" theme is not good (and should be
   redone in styles.xml)
*  Replaces sherlock with Android's native support/appcompat
*  Drop-in jniLibs folder for prebuilt .so library files (see below)
*  Updates dependencies to latest from maven repositories
*  Settings checkboxes->switches
*  Fixes several listview overlapping text areas & other minor UI issues
*  Adds build info to About dialog
*  Only tested on kitkat & lollipop (so caveat emptor)
*  IT'S VERY POSSIBLE SOME FUNCTIONS WILL CRASH-- didn't during testing, but
   if it does, the fixes should be fairly straightforward.

All the above caveats aside, after very limited local testing, the basics seem to
work.  There is no new functionality here, just build updates, removing unneeded stuff,
+s Lollipop integration.  Existing bugs still exist.

To add the .so libraries to the build:

*  Move the prebuilt (or taken from the nightlies) jniLib files to here in the source:

   cSploit/src/org/csploit/android/jniLibs/armeabi-v7a/libcSploitClient.so
   cSploit/src/org/csploit/android/jniLibs/armeabi-v7a/libcSploitCommon.so
   cSploit/src/org/csploit/android/jniLibs/armeabi/libcSploitClient.so
   cSploit/src/org/csploit/android/jniLibs/armeabi/libcSploitCommon.so

They are then put in the apk automatically by Android Studio/gradle.

Change-Id: I02cbf675d5f6daa48646b0d3fddcd4707c78282e

cSploit/src/org/csploit/android/ActionActivity.java
@@ -23,28 +23,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.actionbarsherlock.app.SherlockListActivity;
-import com.actionbarsherlock.view.MenuItem;
-
-import java.util.ArrayList;
-
-import org.csploit.android.R;
-
 import org.csploit.android.core.Plugin;
 import org.csploit.android.core.System;
 import org.csploit.android.gui.dialogs.FinishDialog;
 
-public class ActionActivity extends SherlockListActivity {
+import java.util.ArrayList;
+
+public class ActionActivity extends ActionBarActivity {
     private ArrayList<Plugin> mAvailable = null;
+    private ListView theList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,7 +52,7 @@ public class ActionActivity extends SherlockListActivity {
         Boolean isDark = themePrefs.getBoolean("isDark", false);
 
         if (isDark)
-            setTheme(R.style.Sherlock___Theme);
+            setTheme(R.style.DarkTheme);
         else
             setTheme(R.style.AppTheme);
 
@@ -61,14 +60,36 @@ public class ActionActivity extends SherlockListActivity {
             setTitle("cSploit > " + System.getCurrentTarget());
             setContentView(R.layout.actions_layout);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+            theList = (ListView) findViewById(R.id.android_list);
             mAvailable = System.getPluginsForTarget();
             ActionsAdapter mActionsAdapter = new ActionsAdapter();
+            theList.setAdapter(mActionsAdapter);
+            theList.setOnItemClickListener(new ListView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-            setListAdapter(mActionsAdapter);
-        } else
+                    if (System.checkNetworking(getParent())) {
+                        Plugin plugin = mAvailable.get(position);
+                        System.setCurrentPlugin(plugin);
+
+                        if (plugin.hasLayoutToShow()) {
+                            Toast.makeText(ActionActivity.this, getString(R.string.selected) + getString(plugin.getName()), Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(
+                                    ActionActivity.this,
+                                    plugin.getClass()
+                            ));
+                            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+                        } else
+                            plugin.onActionClick(getApplicationContext());
+                    }
+                }
+            });
+        } else {
             new FinishDialog(getString(R.string.warning), getString(R.string.something_went_wrong), this).show();
+        }
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -84,26 +105,6 @@ public class ActionActivity extends SherlockListActivity {
         }
     }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        if (System.checkNetworking(this)) {
-            Plugin plugin = mAvailable.get(position);
-            System.setCurrentPlugin(plugin);
-
-            if (plugin.hasLayoutToShow()) {
-                Toast.makeText(ActionActivity.this, getString(R.string.selected) + getString(plugin.getName()), Toast.LENGTH_SHORT).show();
-
-                startActivity(new Intent(
-                        ActionActivity.this,
-                        plugin.getClass()
-                ));
-                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
-            } else
-                plugin.onActionClick(this);
-        }
-    }
 
     @Override
     public void onBackPressed() {

build.gradle
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:1.0.0'
+        classpath 'com.android.tools.build:gradle:1.1.3'
     }
 }
 

cSploit/AndroidManifest.xml
@@ -1,12 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="org.csploit.android"
-    android:versionCode="1"
-    android:versionName="1.5.3" >
-
-    <uses-sdk
-        android:minSdkVersion="9"
-        android:targetSdkVersion="18" />
+    package="org.csploit.android">
 
     <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
     <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
@@ -26,12 +20,12 @@
         android:allowBackup="true"
         android:icon="@drawable/ic_launcher"
         android:label="@string/app_name"
-        android:theme="@style/Theme.DSploitActionBarStyle" >
+        android:theme="@style/AppTheme">
         <activity
             android:name=".MainActivity"
             android:configChanges="orientation|screenSize"
             android:label="@string/title_activity_main"
-            android:launchMode="singleTask" >
+            android:launchMode="singleTask">
             <intent-filter>
                 <action android:name="android.intent.action.MAIN" />
 
@@ -41,11 +35,9 @@
 
         <!-- suppress AndroidDomInspection -->
         <service
-            android:name=".core.UpdateChecker"
-            android:enabled="true" />
-        <service
             android:name=".core.UpdateService"
             android:enabled="true" />
+
         <service
             android:name=".core.MultiAttackService"
             android:enabled="true" />
@@ -53,98 +45,81 @@
         <activity
             android:name=".WifiScannerActivity"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".SettingsActivity"
+            android:theme="@style/Theme.AppCompat.Light"
+
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".gui.DirectoryPicker"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".gui.MsfPreferences"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".gui.Console"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".ActionActivity"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.RouterPwn"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.Traceroute"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.PortScanner"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.Inspector"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.Sessions"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.ExploitFinder"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.mitm.MITM"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.mitm.Sniffer"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.mitm.PasswordSniffer"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.mitm.hijacker.Hijacker"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.mitm.hijacker.HijackerWebView"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.LoginCracker"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
         <activity
             android:name=".plugins.PacketForger"
             android:configChanges="orientation|screenSize"
-            android:label="@string/title_activity_main" >
-        </activity>
+            android:label="@string/title_activity_main" />
     </application>
 
 </manifest>

cSploit/build.gradle
@@ -1,30 +1,58 @@
-apply plugin: 'android'
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:1.2.0-beta1'
+    }
+}
+
+def getDate() {
+    return Calendar.getInstance().getTimeInMillis();
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+apply plugin:  'com.android.application'
 
 dependencies {
-    compile fileTree(dir: 'libs', include: '*.jar')
-    compile 'com.android.support:support-v4:19.0.+'
-    compile 'com.actionbarsherlock:actionbarsherlock:4.4.0@aar'
-    compile 'org.apache.commons:commons-compress:1.6'
-    compile 'org.apache.commons:commons-compress:1.6:sources'
+    compile 'com.android.support:support-v4:22.0.0'
+    compile 'com.android.support:appcompat-v7:22.0.0'
+    compile 'org.apache.commons:commons-compress:1.9'
+    compile 'org.apache.commons:commons-compress:1.9:sources'
     compile 'commons-net:commons-net:3.3'
-    compile 'junit:junit:3.8.1'
-    compile 'joda-time:joda-time:1.6.2'
+    compile 'junit:junit:4.12'
+    compile 'joda-time:joda-time:2.7'
     compile 'com.github.zafarkhaja:java-semver:0.8.0'
     compile 'com.sksamuel.diff:diff:1.1.11'
     compile 'org.unbescape:unbescape:1.1.0.RELEASE'
+    compile 'org.msgpack:msgpack:0.6.11'
+    compile 'com.googlecode.juniversalchardet:juniversalchardet:1.0.3'
+    compile 'org.tukaani:xz:1.0'
 }
 
 android {
-    compileSdkVersion 19
-    buildToolsVersion "19.1.0"
+    compileSdkVersion 22
+    buildToolsVersion "22.0.1"
+
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_7
+        targetCompatibility JavaVersion.VERSION_1_7
+    }
 
     packagingOptions {
         exclude 'META-INF/LICENSE.txt'
         exclude 'META-INF/NOTICE.txt'
     }
 
-    lintOptions {
-        abortOnError false
+    defaultConfig {
+        minSdkVersion 9
+        targetSdkVersion 22
+        versionCode 1
+        versionName "1.5.3"
     }
 
     signingConfigs {
@@ -39,7 +67,18 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField "java.util.Date", "BUILD_TIME", "new java.util.Date(" + getDate() + "L)"
+            buildConfigField "String", "BUILD_NAME", "\"" + System.getenv("USER") + "\"";
+            minifyEnabled false
+            shrinkResources false
+            debuggable true
+            jniDebuggable true
+            zipAlignEnabled true
+        }
         release {
+            buildConfigField "java.util.Date", "BUILD_TIME", "new java.util.Date(" + getDate() + "L)"
+            buildConfigField "String", "BUILD_NAME", "\"" + System.getenv("USER") + "\"";
             if (System.getenv("KEYSTORE_FILE") != null) {
                 signingConfig signingConfigs.release
             }
@@ -55,6 +94,7 @@ android {
             renderscript.srcDirs = ['src']
             res.srcDirs = ['res']
             assets.srcDirs = ['assets']
+            jniLibs.srcDir 'src/org/csploit/android/jniLibs'
         }
 
         instrumentTest.setRoot('tests')

cSploit/res/layout/about_dialog.xml
@@ -76,11 +76,29 @@
                 android:layout_width="wrap_content"
                 android:layout_height="wrap_content"
                 android:layout_alignLeft="@+id/textView7"
-                android:layout_alignParentBottom="true"
                 android:layout_alignParentRight="true"
                 android:layout_below="@+id/textView7"
                 android:text="@string/gpl_license"
                 android:textAppearance="?android:attr/textAppearanceSmall"/>
+
+        <TextView
+            android:id="@+id/textView9"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:layout_alignLeft="@+id/textView8"
+            android:layout_below="@+id/textView8"
+            android:text="@string/buildinfo"
+            android:textAppearance="?android:attr/textAppearanceMedium"/>
+
+        <TextView
+            android:id="@+id/buildinfo"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:layout_alignLeft="@+id/textView9"
+            android:layout_alignParentBottom="true"
+            android:layout_alignParentRight="true"
+            android:layout_below="@+id/textView9"
+            android:textAppearance="?android:attr/textAppearanceSmall"/>
     </RelativeLayout>
 
 </ScrollView>

cSploit/res/layout/actions_layout.xml
@@ -22,7 +22,7 @@
 
     <!--suppress AndroidDomInspection -->
     <ListView
-        android:id="@+id/android:list"
+        android:id="@+id/android_list"
         android:layout_width="match_parent"
         android:layout_height="wrap_content"
         android:showDividers="none"

cSploit/res/layout/actions_list_item.xml
@@ -59,8 +59,7 @@
                 android:maxLines="50"
                 android:fontFamily="sans-serif-light"
                 android:textSize="14sp"
-                android:autoLink="all"
-                android:text="lorem ipsum dolor sit amet" />
+                android:autoLink="all" />
         </LinearLayout>
     </LinearLayout>
 </LinearLayout>

commit 4e44374fcb43383531458319dc76f87e7336b3d9

fixes #281 caused by 9eb34156d93543c60b62c3c420551ec717a1513e

cSploit/src/org/csploit/android/ActionActivity.java
@@ -68,7 +68,7 @@ public class ActionActivity extends AppCompatActivity {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                    if (System.checkNetworking(getParent())) {
+                    if (System.checkNetworking(ActionActivity.this)) {
                         Plugin plugin = mAvailable.get(position);
                         System.setCurrentPlugin(plugin);
 