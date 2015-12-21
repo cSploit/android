731f6fea568c4b724bb19542e71bfb60c5532e54
tux-mind <massimo.dragano@gmail.com>
2015-12-21 17:35:17 +0100

    create a BaseLiveListAdapter to adapt live lists to UI

---
+++ cSploit/src/main/java/org/csploit/android/gui/adapters/BaseLiveListAdapter.java
@@ -0,0 +1,141 @@
+package org.csploit.android.gui.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import org.csploit.android.R;
+import org.csploit.android.helpers.GUIHelper;
+
+import java.util.List;
+
+/**
+ * A list adapter that show data from a live list that change
+ */
+public abstract class BaseLiveListAdapter<T, H> extends BaseAdapter {
+
+  private final Activity activity;
+  private final LayoutInflater inflater;
+  private final boolean dark;
+  private final Class<H> holderClass;
+  private List<T> list;
+  private ListView lv;
+
+  public BaseLiveListAdapter(@NonNull Activity activity, @NonNull Class<H> holderClass) {
+    this.activity = activity;
+    dark = GUIHelper.isDarkThemeEnabled();
+    this.holderClass = holderClass;
+    inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    list = getListCopy();
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    View row = convertView;
+    H holder;
+    Object tag = row != null ? row.getTag() : null;
+
+    if (tag == null || !(holderClass.isInstance(tag))) {
+      row = inflater.inflate(getItemLayout(), parent, false);
+
+      onRowCreated(row);
+
+      holder = createHolder(row);
+      row.setTag(holder);
+    } else {
+      holder = (H) tag;
+    }
+
+    final T item = list.get(position);
+
+    updateView(item, holder);
+
+    return row;
+  }
+
+  @Override
+  public int getCount() {
+    return list.size();
+  }
+
+  @Override
+  public Object getItem(int position) {
+    return list.get(position);
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return 0;
+  }
+
+  protected LayoutInflater getInflater() {
+    return inflater;
+  }
+
+  protected void onRowCreated(View row) {
+    if (dark)
+      row.setBackgroundResource(R.drawable.card_background_dark);
+  }
+
+  protected void updateWholeList() {
+    activity.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        list = getListCopy();
+        notifyDataSetChanged();
+      }
+    });
+  }
+
+  protected void updateOneItem(final T item) {
+    if (lv == null) {
+      updateWholeList();
+      return;
+    }
+
+    activity.runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        int start = lv.getFirstVisiblePosition();
+        int end = Math.min(lv.getLastVisiblePosition(), list.size());
+        for (int i = start; i <= end; i++) {
+          if (item == list.get(i)) {
+            View view = lv.getChildAt(i - start);
+            getView(i, view, lv);
+            break;
+          }
+        }
+      }
+    });
+  }
+
+  protected boolean isDark() {
+    return dark;
+  }
+
+  protected Activity getActivity() {
+    return activity;
+  }
+
+  protected List<T> getList() {
+    return list;
+  }
+
+  protected abstract List<T> getListCopy();
+
+  protected abstract @LayoutRes int getItemLayout();
+
+  protected abstract H createHolder(View row);
+
+  protected abstract void updateView(T item, H holder);
+
+  public void setListView(ListView lv) {
+    this.lv = lv;
+  }
+}
--- cSploit/src/main/java/org/csploit/android/gui/adapters/TargetListAdapter.java
+++ cSploit/src/main/java/org/csploit/android/gui/adapters/TargetListAdapter.java
@@ -1,17 +1,12 @@
 package org.csploit.android.gui.adapters;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 import android.text.Html;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import org.csploit.android.R;
@@ -22,65 +17,50 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
-public class TargetListAdapter extends BaseAdapter implements Runnable, Observer {
-
-  private List<Target> list = org.csploit.android.core.System.getTargets();
-  private boolean isDark;
-  private final Activity activity;
-  private ListView lv;
+public class TargetListAdapter extends BaseLiveListAdapter<Target, TargetListAdapter.TargetHolder> implements Observer {
 
   public TargetListAdapter(Activity activity) {
-    this.activity = activity;
-    isDark = System.getSettings().getBoolean("PREF_DARK_THEME", false);
-  }
-
-  public void setListView(ListView lv) {
-    this.lv = lv;
+    super(activity, TargetListAdapter.TargetHolder.class);
   }
 
   @Override
-  public int getCount() {
-    return list.size();
-  }
-
-  @Override
-  public Object getItem(int position) {
-    return list.get(position);
-  }
-
-  @Override
-  public long getItemId(int position) {
-    return R.layout.target_list_item;
-  }
-
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-    View row = convertView;
-    TargetHolder holder;
-    Object tag = row != null ? row.getTag() : null;
-
-    if (row == null || tag == null || !(tag instanceof TargetHolder)) {
-      LayoutInflater inflater = (LayoutInflater) activity
-              .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-      row = inflater.inflate(R.layout.target_list_item, parent, false);
-
-      if (isDark)
-        row.setBackgroundResource(R.drawable.card_background_dark);
-
-      holder = new TargetHolder();
-      holder.itemImage = (ImageView) row.findViewById(R.id.itemIcon);
-      holder.itemTitle = (TextView) row.findViewById(R.id.itemTitle);
-      holder.itemDescription = (TextView) row.findViewById(R.id.itemDescription);
-      holder.portCount = (TextView) row.findViewById(R.id.portCount);
-      holder.portCountLayout = (LinearLayout) row.findViewById(R.id.portCountLayout);
-      if (isDark)
-        holder.portCountLayout.setBackgroundResource(R.drawable.rounded_square_grey);
-      row.setTag(holder);
+  public void update(Observable observable, Object data) {
+    if(data == null || !(data instanceof Target)) {
+      updateWholeList();
     } else {
-      holder = (TargetHolder) tag;
+      updateOneItem((Target) data);
+    }
+  }
+
+  public int indexOf(String id) {
+    List<Target> list = getList();
+    for(int i = 0; i < list.size(); i++) {
+      if(list.get(i).getIdentifier().equals(id)) {
+        return i;
+      }
     }
 
-    final Target target = list.get(position);
+    return -1;
+  }
+
+  @Override
+  protected TargetHolder createHolder(View row) {
+    TargetHolder holder = new TargetHolder();
+
+    holder.itemImage = (ImageView) row.findViewById(R.id.itemIcon);
+    holder.itemTitle = (TextView) row.findViewById(R.id.itemTitle);
+    holder.itemDescription = (TextView) row.findViewById(R.id.itemDescription);
+    holder.portCount = (TextView) row.findViewById(R.id.portCount);
+    holder.portCountLayout = (LinearLayout) row.findViewById(R.id.portCountLayout);
+
+    if (isDark())
+      holder.portCountLayout.setBackgroundResource(R.drawable.rounded_square_grey);
+
+    return holder;
+  }
+
+  @Override
+  protected void updateView(Target target, TargetHolder holder) {
 
     if (target.hasAlias()) {
       holder.itemTitle.setText(Html.fromHtml("<b>"
@@ -89,7 +69,10 @@ public class TargetListAdapter extends BaseAdapter implements Runnable, Observer
     } else {
       holder.itemTitle.setText(target.toString());
     }
-    holder.itemTitle.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), (target.isConnected() ? R.color.app_color : R.color.gray_text)));
+
+    holder.itemTitle.setTextColor(ContextCompat.getColor(
+            getActivity().getApplicationContext(),
+            (target.isConnected() ? R.color.app_color : R.color.gray_text)));
 
     holder.itemTitle.setTypeface(null, Typeface.NORMAL);
     holder.itemImage.setImageResource(target.getDrawableResourceId());
@@ -99,56 +82,16 @@ public class TargetListAdapter extends BaseAdapter implements Runnable, Observer
 
     holder.portCount.setText(String.format("%d", openedPorts));
     holder.portCountLayout.setVisibility(openedPorts < 1 ? View.GONE : View.VISIBLE);
-    return row;
-  }
-
-  public int indexOf(String id) {
-    synchronized (this) {
-      for(int i = 0; i < list.size(); i++) {
-        if(list.get(i).getIdentifier().equals(id)) {
-          return i;
-        }
-      }
-    }
-    return -1;
   }
 
   @Override
-  public void update(Observable observable, Object data) {
-    final Target target = (Target) data;
-
-    if (target == null) {
-      // update the whole list
-      activity.runOnUiThread(this);
-      return;
-    }
-
-    // update only a row, if it's displayed
-    activity.runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        if (lv == null)
-          return;
-        synchronized (this) {
-          int start = lv.getFirstVisiblePosition();
-          int end = Math.min(lv.getLastVisiblePosition(), list.size());
-          for (int i = start; i <= end; i++)
-            if (target == list.get(i)) {
-              View view = lv.getChildAt(i - start);
-              getView(i, view, lv);
-              break;
-            }
-        }
-      }
-    });
+  protected List<Target> getListCopy() {
+    return System.getTargets();
   }
 
   @Override
-  public void run() {
-    synchronized (this) {
-      list = System.getTargets();
-    }
-    notifyDataSetChanged();
+  protected int getItemLayout() {
+    return R.layout.target_list_item;
   }
 
   class TargetHolder {
