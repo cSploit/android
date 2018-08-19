e3b0dd075d594169e4387abd65ef3f0355bc4710
fattire <f4ttire@gmail.com>
2018-08-19 14:49:45 -0700

    Add check to make sure WAKE_LOCK perm is available + delcare in manifest
    This was an issue in ACRA.  Thanks to vczilla & gregd for chasing it down.
    See issue #941

--- cSploit/src/main/java/org/csploit/android/MainActivity.java
+++ cSploit/src/main/java/org/csploit/android/MainActivity.java
@@ -70,9 +70,13 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_PHONE_STATE)
-                != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+              Manifest.permission.WAKE_LOCK)
+              != PackageManager.PERMISSION_GRANTED)
+      {
           ActivityCompat.requestPermissions(this,
-                  new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE},
+                  new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE,
+                  Manifest.permission.WAKE_LOCK},
                   MY_PERMISSIONS_WANTED);
         }
     }
